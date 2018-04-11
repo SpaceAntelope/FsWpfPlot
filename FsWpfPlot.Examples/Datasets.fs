0cf9f82e21af8576daa690d4d352280d0c11e462
Ares Terzopoulos <cernunos_kav@msn.com>
2018-04-11 09:02:10 +0300

    Added some wb provider data to use with examples

---
+++ FsWpfPlot.Examples/Datasets.fs
@@ -0,0 +1,81 @@
+﻿namespace FsWpfPlot.Examples.Datasets
+
+open FSharp.Data
+open Newtonsoft.Json
+
+module Wb =
+
+    type ExampleCsv = CsvProvider<Schema="Index(int),Country(string),Surface(float option),Pop2000(float option),Pop2010(float option),Pollution(float option),EducationM(float option),EducationF(float option)", HasHeaders = false>
+
+    type ExampleData = {
+        Index: int
+        Country : string
+        Surface: float option
+        Pop2000: float option
+        Pop2010: float option   
+        Pollution: float option
+        EducationM: float option
+        EducationF: float option
+    }
+
+    let GenerateExampleData() = 
+        let NanToOpt num = if System.Double.IsNaN num then None else Some(num)
+        let mutable cnt = 0
+        let inc() = cnt <- cnt + 1; cnt
+        [| for country in WorldBankData.GetDataContext().Countries -> {
+                    Index = inc()
+                    Country = country.Name
+                    Surface = country.Indicators.``Surface area (sq. km)``.[2000]  |> NanToOpt
+                    Pop2000 = country.Indicators.``Population, total``.[2000] |> NanToOpt
+                    Pop2010 = country.Indicators.``Population, total``.[2010] |> NanToOpt
+                    Pollution = country.Indicators.``CO2 emissions (kt)``.[2000] |> NanToOpt
+                    EducationM = country.Indicators.``School enrollment, secondary, male (% gross)`` .[2000] |> NanToOpt
+                    EducationF = country.Indicators.``School enrollment, secondary, female (% gross)`` .[2000] |> NanToOpt
+        }|]
+
+    let storeDataToCache (data : ExampleData[]) =
+        data 
+        |> JsonConvert.SerializeObject 
+        |> fun str -> System.IO.File.WriteAllText("wb.data.json", str)
+
+        data 
+        |> Seq.map (fun item -> ExampleCsv.Row(item.Index, item.Country, item.Surface, item.Pop2000, item.Pop2010, item.Pollution, item.EducationM, item.EducationF))
+        |> fun rows -> 
+            System.IO.File.AppendAllText("wb.data.csv", ExampleCsv(rows).SaveToString())
+
+    let GetExampleDataFromCache() =
+        System.IO.Directory.GetFiles(__SOURCE_DIRECTORY__ ,"wb.data.*")
+        |> List.ofArray
+        |> function 
+        | [] -> 
+            let data = GenerateExampleData()
+            data |> storeDataToCache
+            data
+        | filename::_ ->            
+            match System.IO.Path.GetExtension(filename) with 
+            | ".csv"-> 
+                    filename
+                    |> ExampleCsv.Load
+                    |> fun csv -> csv.Rows
+                    |> Seq.map (fun row ->
+                            {
+                                Index = row.Index
+                                Country = row.Country
+                                Surface = row.Surface
+                                Pop2000 = row.Pop2000
+                                Pop2010 = row.Pop2010
+                                Pollution = row.Pollution
+                                EducationM = row.EducationM
+                                EducationF = row.EducationF
+                            })
+                    |> Array.ofSeq
+            | ".json" -> 
+                    filename
+                    |> System.IO.File.ReadAllText
+                    |> JsonConvert.DeserializeObject<ExampleData[]>
+            | ext -> failwith ("I do not know what to do with " +  ext)
+
+    let GetExampleData() = GetExampleDataFromCache()
+
+    
+
--- FsWpfPlot.Examples/Program.fs
+++ FsWpfPlot.Examples/Program.fs
@@ -1,48 +1,53 @@
-﻿open FsWpfPlot.Models
+﻿namespace FsWpfPlot.Examples
+
+open FsWpfPlot.Models
 open FsWpfPlot.Types
+open FsWpfPlot.Examples.Datasets
 open System
 open System.Windows.Shapes
 open System.Windows
 open System.Windows.Media.Media3D
 
-
-
-
-let plot3d (f: PlotFunction) = 
-    let model = Plot3DModel(Resolution = 200)
-    model.setRangeX -1. 1.
-    model.setRangeY -1. 1.
-    model.FuncZ <- Some(f)
+module Main = 
+    let plot3d (f: PlotFunction) = 
+        let model = Plot3DModel(Resolution = 200)
+        model.setRangeX -1. 1.
+        model.setRangeY -1. 1.
+        model.FuncZ <- Some(f)
         
-    let plot3d = FsWpfPlot.Views.SurfacePlotContainerProxy(model)
-    printfn "%A" <| plot3d.Show()
+        let plot3d = FsWpfPlot.Views.SurfacePlotContainerProxy(model)
+        printfn "%A" <| plot3d.Show()
 
-let plot2d (f : ChartFunction) =
-    let model = Plot2DModel(Resolution = 100)
-    model.setRangeX -3. 3.
-    model.Func <- Some(f)
+    let plot2d (f : ChartFunction) =
+        let model = Plot2DModel(Resolution = 100)
+        model.setRangeX -3. 3.
+        model.Func <- Some(f)
 
-    let plot2d = FsWpfPlot.Views.Plot2DContainerProxy(model)
-    printfn "%A" <| plot2d.Show()
+        let plot2d = FsWpfPlot.Views.Plot2DContainerProxy(model)
+        printfn "%A" <| plot2d.Show()
 
-[<EntryPoint>]
-[<STAThread>]
-let main argv = 
+    [<EntryPoint>]
+    [<STAThread>]
+    let main argv = 
 
-    let inline signf x = (sign x) |> float
+        let inline signf x = (sign x) |> float
 
-    let radial : PlotFunction = fun x y -> sqrt (x**2. + y**2.)
-    let windmill : PlotFunction = fun x y -> signf(x*y)*signf(1. - (x*9.)**2. + (y*9.)**2.)/9.
-    let fences = fun x y -> 0.75 * exp(((x*5.)**2. * (y*5.)**2.)**2.)
-    let ripple : PlotFunction = fun x y -> sin ( 10.*(x**2. + y**2. )/10.) 
-    let ripple' : PlotFunction = fun x y -> sin ( 10.*(x**2. + y**2.)/10.) + tanh(x*y) - radial x y
-    let torus : PlotFunction = fun x y -> (0.4**2. - (0.6-(x**2. + y**2.)**0.5)**2.)**0.5
-    let tube : PlotFunction = fun x y -> 1.0/(15.*(x**2. + y**2.))
-    
-    //plot3d tube
-    plot2d (fun x -> sin x)
+        let radial : PlotFunction = fun x y -> sqrt (x**2. + y**2.)
+        let windmill : PlotFunction = fun x y -> signf(x*y)*signf(1. - (x*9.)**2. + (y*9.)**2.)/9.
+        let fences = fun x y -> 0.75 * exp(((x*5.)**2. * (y*5.)**2.)**2.)
+        let ripple : PlotFunction = fun x y -> sin ( 10.*(x**2. + y**2. )/10.) 
+        let ripple' : PlotFunction = fun x y -> sin ( 10.*(x**2. + y**2.)/10.) + tanh(x*y) - radial x y
+        let torus : PlotFunction = fun x y -> (0.4**2. - (0.6-(x**2. + y**2.)**0.5)**2.)**0.5
+        let tube : PlotFunction = fun x y -> 1.0/(15.*(x**2. + y**2.))
+        let curve : PlotFunction = fun x y-> x - 1.6 * cos(24.*x)
 
-    printfn "Done."
-    
-    0
+        plot2d (sin>>cos>>tan)
+        //plot2d (curve)
+
+        //Wb.GetExampleData() |> Array.map (printfn "%A")
+
+
+
+        printfn "Done."    
+        0
     
